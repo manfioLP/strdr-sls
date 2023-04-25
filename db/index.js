215c448dbc88c309774f1eb667c264d09a2f6077
Leopoldo Manfio <leopoldo.manfio@iovlabs.org>
2023-04-25 02:18:07 -0300

    add env files and db connection

---
+++ db/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise; // so .thens will work properly on handlers
+
+let isConnected;
+
+require('dotenv').config({ path: path.resolve('.env') });
+
+const connectionString = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PWD}@cluster0.dnqzeis.mongodb.net/?retryWrites=true&w=majority`;
+
+const connectToDatabase = () => {
+  if (isConnected) {
+    console.log('=> using existing database connection');
+    return Promise.resolve();
+  }
+
+  console.log('=> using new database connection');
+  return mongoose.connect(connectionString)
+    .then((db) => {
+      isConnected = db.connections[0].readyState;
+    });
+};
+
+const closeConnection = () => {
+  isConnected = false;
+  return mongoose.disconnect();
+};
+
+module.exports = {
+  connectToDatabase,
+  closeConnection,
+};
