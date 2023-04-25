f2879a4bb5f44a2e29078edefe7768f36b9ba7bb
Leopoldo Manfio <leopoldo.manfio@iovlabs.org>
2023-04-25 19:15:37 -0300

    add drop collection function for test purpose

--- db/index.js
+++ db/index.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise; // so .thens will work properly on handlers
-
 let isConnected;
 
 require('dotenv').config({ path: path.resolve('.env') });
@@ -11,11 +9,11 @@ const connectionString = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.
 
 const connectToDatabase = () => {
   if (isConnected) {
-    console.log('=> using existing database connection');
+    console.log('... using existing database connection');
     return Promise.resolve();
   }
 
-  console.log('=> using new database connection');
+  console.log('... using new database connection');
   return mongoose.connect(connectionString)
     .then((db) => {
       isConnected = db.connections[0].readyState;
@@ -27,7 +25,25 @@ const closeConnection = () => {
   return mongoose.disconnect();
 };
 
+// for tests purposes only
+const dropCollection = async (collectionName) => {
+  if (!isConnected) {
+    console.error('DB Disconnected, first connect to DB to drop a collection');
+    return { dropped: false, msg: 'DB Disconnected, first connect to DB to drop a collection' };
+  }
+  if (process.env.NODE_ENV === 'PRODUCTION') {
+    console.error('Cant drop a PRODUCTION DB');
+    return { dropped: false, msg: 'Cannot drop a PRODUCTION DB' };
+  }
+
+  const drop = await mongoose.connection.db.dropCollection(collectionName);
+  return {
+    drop, msg: `Collection ${collectionName} dropped!`,
+  };
+};
+
 module.exports = {
   connectToDatabase,
   closeConnection,
+  dropCollection,
 };
