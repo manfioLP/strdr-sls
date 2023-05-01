01ebbefbc3a65d8edede8ea67151c03d610d85fe
Leopoldo Manfio <leopoldo.manfio@iovlabs.org>
2023-05-01 15:15:21 -0300

    add seed to users

---
+++ data-import/users/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+// const { ObjectId } = mongoose.Schema.Types;
+
+module.exports = [
+  {
+    _id: new mongoose.Types.ObjectId('644ffee315a89ad5befca503'),
+    username: 'leo123',
+    email: 'leo@poldo.pro',
+    phone: '+5548998257777',
+    name: 'Leo Tester',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('645000e715a89ad5befca504'),
+    username: 'poldo22',
+    email: 'poldo@poldo.pro',
+    phone: '+5548998257171',
+    name: 'Poldo Tester',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6450010315a89ad5befca505'),
+    username: 'kayutcho',
+    email: 'kayo@poldo.pro',
+    phone: '+13059984567777',
+    name: 'Kayo Tester',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6450010f15a89ad5befca506'),
+    username: 'nazoka',
+    email: 'nati@poldo.pro',
+    name: 'Natalia Tester',
+  },
+];
