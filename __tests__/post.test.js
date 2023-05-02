5605e445b46f46f53a49227b662b735ebf7aebc2
Leopoldo Manfio <leopoldo.manfio@iovlabs.org>
2023-05-02 19:54:22 -0300

    implement create original post

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -1,31 +1,57 @@
-const lambdaTester = require('lambda-tester');
+jest.setTimeout(12000); // sometimes depending on network Atlas connection can take ~10secs
 
 const { closeConnection, dropCollection } = require('../db');
+const seedUsers = require('../data-import/seed-users');
 
 const createOriginalPostL = require('../handlers/posts/original');
 
-const postDTO = {
-  user: '',
-  text: 'alea jacta est',
+const context = {
+  awsRequestId: 'ckb8j7s4q0002qjr9azyw2xmv',
+  callbackWaitsForEmptyEventLoop: false,
+  clientContext: null,
+  functionName: 'robot-delivery-dev-addRobot',
+  functionVersion: '$LATEST',
 };
 
-describe('Robot Basics', () => {
-  beforeAll((done) => {
-    process.env.DB_NAME = 'strider-test';
-    done();
+const users = require('../data-import/users');
+
+const postDTO = (user, text = 'alea jacta est') => ({ user, text });
+
+const expectPostProperties = (p) => {
+  expect(p).toHaveProperty('user');
+  expect(p).toHaveProperty('type');
+  expect(p).toHaveProperty('text');
+  expect(p).toHaveProperty('_id');
+};
+
+describe('Post Basics', () => {
+  const ENV = process.env;
+  process.env.DB_NAME = 'strider-test';
+  beforeAll(async () => {
+    // jest.resetModules();
+    // process.env = { ...ENV, DB_NAME: 'strider-test' };
+    // process.env.DB_NAME = 'strider-test';
+    console.log('BEFORE ALL');
+    await seedUsers('strider-test', process.env.MONGO_USERNAME, process.env.MONGO_PWD);
   });
 
-  test('Create Original Post', async () => lambdaTester(createOriginalPostL)
-    .event({
-      body: JSON.stringify({ ...postDTO }),
-    })
-    .expectResult((r) => {
-      console.log('r', r);
-      expect(r).toHaveProperty('statusCode', 200);
-    }));
+  // beforeEach();
+
+  test('Create Original Post for User0', async () => {
+    process.env.DB_NAME = 'strider-test';
+    const response = await createOriginalPostL({
+      body: JSON.stringify(postDTO(users[0])),
+    }, context);
+    expect(response).toHaveProperty('statusCode', 200);
+
+    const post = JSON.parse(response.body);
+    expectPostProperties(post);
+    expect(post).toHaveProperty('user', users[0]._id.toString());
+    expect(post).toHaveProperty('text');
+  });
 
   afterAll(async () => {
-    await dropCollection('robots');
+    // await dropCollection('posts');
     closeConnection();
   });
 });
---
+++ handlers/posts/original.js
@@ -0,0 +1,29 @@
+const { connectToDatabase } = require('../../db');
+const { Post } = require('../../db/models');
+
+module.exports = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    await connectToDatabase();
+
+    const post = await Post.create({
+      ...JSON.parse(event.body),
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(post),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+  } catch (err) {
+    const msg = 'Could not create the post.';
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: JSON.stringify({ msg, err }),
+    };
+  }
+};
